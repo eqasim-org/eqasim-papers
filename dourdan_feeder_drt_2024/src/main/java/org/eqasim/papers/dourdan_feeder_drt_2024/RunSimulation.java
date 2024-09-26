6ac071dfaf3c991db6a621b6563dddc256dbfe53
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2024-09-26 19:27:06 +0200

    feat: custom run script

--- dourdan_feeder_drt_2024/src/main/java/org/eqasim/papers/dourdan_feeder_drt_2024/RunSimulation.java
+++ dourdan_feeder_drt_2024/src/main/java/org/eqasim/papers/dourdan_feeder_drt_2024/RunSimulation.java
@@ -1,9 +1,41 @@
 package org.eqasim.papers.dourdan_feeder_drt_2024;
 
+import org.eqasim.core.scenario.validation.VehiclesValidator;
+import org.eqasim.core.simulation.analysis.EqasimAnalysisModule;
+import org.eqasim.core.simulation.mode_choice.EqasimModeChoiceModule;
+import org.eqasim.ile_de_france.IDFConfigurator;
+import org.eqasim.ile_de_france.mode_choice.IDFModeChoiceModule;
+import org.eqasim.papers.dourdan_feeder_drt_2024.mode_choice.ModeChoiceModule;
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.CommandLine;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
 
 public class RunSimulation {
     public static void main(String[] args) throws CommandLine.ConfigurationException {
-        org.eqasim.ile_de_france.RunSimulation.main(args);
+        CommandLine cmd = new CommandLine.Builder(args) //
+                .requireOptions("config-path") //
+                .allowPrefixes("mode-choice-parameter", "cost-parameter") //
+                .build();
+
+        IDFConfigurator configurator = new IDFConfigurator();
+        Config config = ConfigUtils.loadConfig(cmd.getOptionStrict("config-path"), configurator.getConfigGroups());
+        configurator.addOptionalConfigGroups(config);
+        cmd.applyConfiguration(config);
+        VehiclesValidator.validate(config);
+
+        Scenario scenario = ScenarioUtils.createScenario(config);
+        configurator.configureScenario(scenario);
+        ScenarioUtils.loadScenario(scenario);
+        configurator.adjustScenario(scenario);
+
+        Controler controller = new Controler(scenario);
+        configurator.configureController(controller);
+        controller.addOverridingModule(new EqasimAnalysisModule());
+        controller.addOverridingModule(new EqasimModeChoiceModule());
+        controller.addOverridingModule(new ModeChoiceModule(cmd));
+        controller.run();
     }
 }
