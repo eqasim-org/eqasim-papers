2226c5b294bfdc1d7ef64c19a8a7b8a9288dcde0
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2025-04-27 20:12:37 +0200

    feat: CustomConfigurator

---
+++ gpe_2025/src/main/java/org/eqasim/papers/gpe2025/CustomConfigurator.java
@@ -0,0 +1,34 @@
+package org.eqasim.papers.gpe2025;
+
+import org.eqasim.ile_de_france.IDFConfigurator;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.core.config.CommandLine;
+
+
+public class CustomConfigurator extends IDFConfigurator {
+
+    public static final double OFFSET = 3600;
+
+    public CustomConfigurator(CommandLine cmd) {
+        super(cmd);
+    }
+
+    public void adjustScenario(Scenario scenario) {
+        super.adjustScenario(scenario);
+        double lastPersonEndTime = scenario.getPopulation().getPersons().values().stream()
+                .mapToDouble(p -> p.getSelectedPlan().getPlanElements().stream().mapToDouble(planElement -> {
+                    if (planElement instanceof Activity activity) {
+                        return activity.getEndTime().orElse(activity.getStartTime().orElse(-1));
+                    } else if (planElement instanceof Leg leg) {
+                        return leg.getDepartureTime().orElse(-1) + leg.getTravelTime().orElse(0);
+                    }
+                    throw new IllegalStateException("Unknown planElement type");
+                }).max().orElse(-1))
+                .filter(t -> t > 0)
+                .max().getAsDouble();
+
+        scenario.getConfig().qsim().setEndTime(Math.max(scenario.getConfig().qsim().getEndTime().orElse(-1), lastPersonEndTime + OFFSET));
+    }
+}
