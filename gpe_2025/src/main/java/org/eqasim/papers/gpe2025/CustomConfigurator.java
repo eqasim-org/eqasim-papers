df16a4110760197944b0b46dc56c85a8bcfb0713
tkchouaki <tkchouaki@gmail.com>
2025-06-19 11:23:42 +0200

    feat: enabling transitWithAbstractAccess

--- gpe_2025/src/main/java/org/eqasim/papers/gpe2025/CustomConfigurator.java
+++ gpe_2025/src/main/java/org/eqasim/papers/gpe2025/CustomConfigurator.java
@@ -1,10 +1,14 @@
 package org.eqasim.papers.gpe2025;
 
+import org.eqasim.core.simulation.modes.transit_with_abstract_access.mode_choice.TransitWithAbstractAccessModeAvailabilityWrapper;
 import org.eqasim.ile_de_france.IDFConfigurator;
+import org.eqasim.ile_de_france.mode_choice.IDFModeAvailability;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
+import org.matsim.contribs.discrete_mode_choice.model.mode_availability.ModeAvailability;
 import org.matsim.core.config.CommandLine;
+import org.matsim.core.controler.AbstractModule;
 
 import java.util.OptionalDouble;
 
@@ -15,6 +19,12 @@ public class CustomConfigurator extends IDFConfigurator {
 
     public CustomConfigurator(CommandLine cmd) {
         super(cmd);
+        registerModule(new AbstractModule() {
+            @Override
+            public void install() {
+                bind(ModeAvailability.class).toProvider(() -> new TransitWithAbstractAccessModeAvailabilityWrapper(getConfig(), new IDFModeAvailability()));
+            }
+        });
     }
 
     public void adjustScenario(Scenario scenario) {
