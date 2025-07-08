ad667a2516a2d8b92aa0a4ebe98f2a9649c345ae
tkchouaki <tkchouaki@gmail.com>
2025-07-08 18:55:07 +0200

    chore: more testing

--- gpe_2025/src/test/java/org/eqasim/papers/gpe2025/Gpe2025Tests.java
+++ gpe_2025/src/test/java/org/eqasim/papers/gpe2025/Gpe2025Tests.java
@@ -8,6 +8,8 @@ import java.net.URL;
 import java.nio.file.Paths;
 
 import org.eqasim.core.scenario.routing.RunPopulationRouting;
+import org.eqasim.core.simulation.modes.transit_with_abstract_access.utils.AdaptConfigForTransitWithAbstractAccess;
+import org.eqasim.core.simulation.modes.transit_with_abstract_access.utils.CreateAbstractAccessItemsForTransitLines;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -45,6 +47,29 @@ public class Gpe2025Tests {
                 "--config:plans.inputPlansFile", routedPlansPath,
                 "--config:controller.outputDirectory", Paths.get(basePath, "output", "simulation").toString(),
                 "--config:controller.lastIteration", "0"
+        });
+
+        CreateAbstractAccessItemsForTransitLines.main(new String[]{
+                "--transit-schedule-path", Paths.get(basePath, "input", String.format("%stransit_schedule.xml.gz", prefix)).toString(),
+                "--route-modes", "rail",
+                "--radius", "1000",
+                "--average-speed", "4.5",
+                "--access-type", "bus",
+                "--use-routed-distance", "false",
+                "--output-path", Paths.get(basePath, "input", String.format("%sfeeders.xml", prefix)).toString()
+        });
+
+        AdaptConfigForTransitWithAbstractAccess.main(new String[] {
+                "--input-config-path", Paths.get(basePath, "input", String.format("%sconfig.xml", prefix)).toString(),
+                "--output-config-path", Paths.get(basePath, "input", String.format("%sconfig_feeders.xml", prefix)).toString(),
+                "--mode-name", "transitWithAbstractAccess",
+                "--accesses-file-path", Paths.get(basePath, "input", String.format("%sfeeders.xml", prefix)).toString()
+        });
+
+        RunSimulation.main(new String[]{
+                "--config-path", Paths.get(basePath, "input", String.format("%sconfig_feeders.xml", prefix)).toString(),
+                "--config:controller.outputDirectory", Paths.get(basePath, "output", "simulation_feeders").toString(),
+                "--config:controller.lastIteration", "1"
         });
     }
 
