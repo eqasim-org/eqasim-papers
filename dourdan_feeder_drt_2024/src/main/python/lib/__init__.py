d93a24c97669eba83c354570c5bcddb5adc2ff47
tkchouaki <tkchouaki@gmail.com>
2025-10-27 09:58:40 +0100

    chore: more precise error

--- dourdan_feeder_drt_2024/src/main/python/lib/__init__.py
+++ dourdan_feeder_drt_2024/src/main/python/lib/__init__.py
@@ -328,8 +328,13 @@ class PipelineConfig:
             assert simulation_configs[demand_identification_simulation_config.hash_code].hash_code == simulation_configs[demand_identification_simulation_config.hash_code].demand_source
         return simulation_configs
 
+    def hash_to_config(self, hash_code):
+        if hash_code not in self.simulation_configs:
+            raise Exception("Simulation config with hash '%s' not found among the %d configs" % (hash_code, len(self.simulation_configs)))
+        return self.simulation_configs[hash_code]
+
     def get_simulation_inputs(self, hash_code, demand_identification, **kwargs):
-        simulation_config = self.simulation_configs[hash_code]
+        simulation_config = self.hash_to_config(hash_code)
         inputs = dict(config=self.get_deployment_scenario_config_path(simulation_config.deployment_scenario))
         inputs["vehicles"] = "%s/%d_%d.xml" % (self.area_vehicles_files_location, simulation_config.fleet_size, simulation_config.services_parameters_values["vehicle_capacity"])
 
@@ -344,7 +349,7 @@ class PipelineConfig:
         return inputs
 
     def get_simulation_args(self, hash_code):
-        simulation_config = self.simulation_configs[hash_code]
+        simulation_config = self.hash_to_config(hash_code)
         args = list()
         detour_factor = simulation_config.services_parameters_values["detour_factor"]
         detour_factor_value = float(detour_factor[1:-1])
